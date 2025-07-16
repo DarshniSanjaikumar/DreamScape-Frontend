ec3e7e1fcb9ff9dedf4faafa0cbd56c657ce1306
DANNE SANDEEP <119658287+DarshniSanjaikumar@users.noreply.github.com>
2025-07-16 11:28:00 +0530

    page not founf update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/', // Ensures correct routing on Render
   plugins: [react()],
   build: {
     rollupOptions: {
